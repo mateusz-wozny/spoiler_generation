3c697b77f4c67c303f0c7a4da248ebe935937939
Mateusz Woźny <mateusz15wozny@gmail.com>
2023-07-19 20:00:57 +0200

    add prepare dataset scripts

---
+++ spoiler_generation/llms/prepare_dataset.py
@@ -0,0 +1,37 @@
+import os
+import pandas as pd
+from datasets import load_dataset
+import sys
+
+from spoiler_generation.utils.dataset_class import Dataset
+
+
+def main(args):
+    dataset = load_dataset(
+        "MateuszW/spoiler_generation",
+        data_files={
+            "train": "clickbait_spoiling_data/train.jsonl",
+            "validation": "clickbait_spoiling_data/validation.jsonl",
+        },
+    )
+    questions = load_dataset(
+        "MateuszW/spoiler_generation",
+        data_files={
+            "train": "generated_questions/train_questions.csv",
+            "validation": "generated_questions/validation_questions.csv",
+        },
+    )
+    for part in ["train", "validation"]:
+        part_dataset = Dataset(pd.DataFrame(dataset[part]), explode=True)
+
+        part_dataset._df["postText"] = (
+            part_dataset._df["postText"] + ". " + questions[part]["generated_questions"]
+        )
+
+        part_dataset.prepare_data_for_llm_training(
+            save_path=os.path.join(args[0], f"{part}.json")
+        )
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
